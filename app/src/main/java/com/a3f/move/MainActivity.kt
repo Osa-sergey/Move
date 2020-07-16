d20dfa0b6ee66fd355997c05a4eb47cc94baa3d7
Ovchinnikov Sergey <owchin.s@yandex.ru>
2020-07-16 17:25:53 +0300

    -Добавил нижнее меню.

--- app/src/main/java/com/a3f/move/MainActivity.kt
+++ app/src/main/java/com/a3f/move/MainActivity.kt
@@ -2,10 +2,15 @@ package com.a3f.move
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var bottomNavMenu: BottomNavigationView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        bottomNavMenu = findViewById(R.id.bottom_nav_view)
     }
 }
